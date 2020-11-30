8a9462928aea0fdba5a3b2221debac8d57cb590a
akshpnwr <akashpanwar9976@gmail.com>
2020-11-30 12:43:36 +0530

    add update functionality

--- book-store/src/main/java/book/controller/BookController.java
+++ book-store/src/main/java/book/controller/BookController.java
@@ -1,7 +1,5 @@
 package book.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +27,12 @@ public class BookController
 	
 	@Autowired
 	SaleService saleService;
+	
+	@RequestMapping("/")
+	public String index()
+	{
+		return "home";
+	}
 	
 	@RequestMapping("/login")
 	public String login()
